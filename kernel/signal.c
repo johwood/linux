8d408e6856d1da2bd98e9db5411742f936f4e7b9
John Wood <john.wood@gmx.com>
2020-09-19 18:17:23 +0200

    security: Add LSM hook at the point where a task gets a fatal signal
    Add a security hook that allows a LSM to be notified when a task gets a
    fatal signal. This patch is a previous step on the way to compute the
    task crash period by the "brute" LSM (linux security module to detect
    and mitigate fork brute force attack against vulnerable userspace
    processes).
    
    Signed-off-by: John Wood <john.wood@gmx.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2787,6 +2787,7 @@ relock:
 		/*
 		 * Anything else is fatal, maybe with a core dump.
 		 */
+		security_task_fatal_signal(&ksig->info);
 		current->flags |= PF_SIGNALED;
 
 		if (sig_kernel_coredump(signr)) {
